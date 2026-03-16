3259cc72d30a7fe6ac8c5355882a0cf7e991c126
Luana Beltrán <luanabeltran.j@gmail.com>
2026-03-16 14:19:57 -0400

    FINALrobot_rpg.py
    LO LOGRAMOS (YEIIIII)

--- robot_rpg.py
+++ robot_rpg.py
@@ -136,6 +136,34 @@ def posicion_power_up(robot, meta, bloqueos):
         if casilla_power != robot and casilla_power != meta and casilla_power not in bloqueos:
             return casilla_power
 
+def generacion_de_mapa(robot, power_up, bloqueos):
+    matriz = [
+    [(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (0,6), (0,7), (0,8), (0,9)], 
+    [(1,0), (1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8), (1,9)],
+    [(2,0), (2,1), (2,2), (2,3), (2,4), (2,5), (2,6), (2,7), (2,8), (2,9)],
+    [(3,0), (3,1), (3,2), (3,3), (3,4), (3,5), (3,6), (3,7), (3,8), (3,9)],
+    [(4,0), (4,1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7), (4,8), (4,9)],
+    [(5,0), (5,1), (5,2), (5,3), (5,4), (5,5), (5,6), (5,7), (5,8), (5,9)],
+    [(6,0), (6,1), (6,2), (6,3), (6,4), (6,5), (6,6), (6,7), (6,8), (6,9)],
+    [(7,0), (7,1), (7,2), (7,3), (7,4), (7,5), (7,6), (7,7), (7,8), (7,9)],
+    [(8,0), (8,1), (8,2), (8,3), (8,4), (8,5), (8,6), (8,7), (8,8), (8,9)],
+    [(9,0), (9,1), (9,2), (9,3), (9,4), (9,5), (9,6), (9,7), (9,8), (9,9)]]
+    for filas in matriz:
+        contenido = 0
+        for celda in filas:
+            if celda not in bloqueos:
+                if celda != robot:
+                    if celda != power_up:
+                        contenido = "⬜"
+                    else:
+                        contenido = "⚡"
+                else:
+                    contenido = "🤖"
+            else:
+                contenido = "🚫"
+            print(contenido, end = "")
+        print("\n")
+
 def iniciar():
     robot = posicion_inicial_robot()
     meta = posicion_meta()
@@ -164,7 +192,7 @@ def main():
 
     if robot and meta and bloqueos:
         while not comprobacion_posicion(robot, meta):
-
+            generacion_de_mapa(robot, power, bloqueos)
             x, y = robot
             mov = input("¿A donde quieres ir? (W/A/S/D/H) : ")
 
