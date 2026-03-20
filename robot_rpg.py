275f8087ce9492334756d39fd7ce28313ef3e199
abi-lab01 <vivianncond@gmail.com>
2026-03-19 20:59:32 -0400

    Update robot_rpg.py
    hicimos con luana comentarios para mejor compresion

--- robot_rpg.py
+++ robot_rpg.py
@@ -134,17 +134,18 @@ def generar_power_ups(prohibidas, cantidad=4):
 
 # ── INTERFAZ TKINTER ──────────────────────────────────────────────
 
-class RobotGUI:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Robot Navigator 🤖")
-        self.root.configure(bg="#1e1e2e")
-        self._nueva_partida()
-        self._build_ui()
+class RobotGUI: # Todo lo que este adentro sera un robotGUI, (Esta clase representa la ventana gráfica del juego y lo gestiona.)
+    def __init__(self, root):#Es el constructor: se ejecuta automáticamente cuando creas un objeto RobotGUI. Recibe root, que es la ventana principal de Tkinter.
+        self.root = root #self root guarda a la ventana del juego como unico
+        self.root.title("Robotsito 🤖")
+        self.root.configure(bg="#bc75e0")
+        self._nueva_partida()#Inicia la partida
+        self._build_ui()#Construye todos los elementos visuales botones , etc
         for tecla, mov in [("<w>","W"),("<a>","A"),("<s>","S"),("<d>","D"),("<h>","H")]:
-            self.root.bind(tecla, lambda e, m=mov: self._mover(m))
-            self.root.bind(tecla.upper(), lambda e, m=mov: self._mover(m))
-
+            #son sintaxis de tkinter para teclas especiales, con bind (<>)Y CADA SE VINCULA SEGUN EL MOV QUE SEA 
+            self.root.bind(tecla, lambda e, m=mov: self._mover(m))#e=evento
+            self.root.bind(tecla.upper(), lambda e, m=mov: self._mover(m))# Lmbda es una "funcion" anonima y pequenia de un solo uso
+#lambda permite hacer una funcion sin hacer una mas aparte
     def _nueva_partida(self):
         self.robot = posicion_inicial_robot()
         self.meta  = posicion_meta()
@@ -156,38 +157,38 @@ class RobotGUI:
         self.power_activo = False
         self.saltos       = 1
         self.ganado       = False
+#Es lo mismo que hicimos en la terminal (ariiba)
+    def _build_ui(self):#construye los widgets
+        tk.Label(self.root, text="🤖 Robotsito", bg="#bc75e0", #color fondo letras
+                 fg="white", font=("Segoe UI", 14, "bold")).pack(pady=8) # titulo y posicion en px de titulo
 
-    def _build_ui(self):
-        tk.Label(self.root, text="🤖 Robot Navigator", bg="#1e1e2e",
-                 fg="white", font=("Segoe UI", 14, "bold")).pack(pady=8)
-
-        grid_f = tk.Frame(self.root, bg="#1e1e2e")
-        grid_f.pack()
-        self.celdas = {}
+        grid_f = tk.Frame(self.root, bg="#1e1e2e") #color de los bordes de la matriz
+        grid_f.pack() # visualiza las casillas
+        self.celdas = {} #diccionario vacio y agregue las celdas x,y
         for f in range(10):
-            for c in range(10):
-                lbl = tk.Label(grid_f, text="⬜", width=3, height=1,
-                               font=("Segoe UI Emoji", 14), bg="#f8f9fa", relief="flat")
-                lbl.grid(row=f, column=c, padx=1, pady=1)
-                self.celdas[(f, c)] = lbl
+            for c in range(10):#lenguaje the thinker misma logica
+                lbl = tk.Label(grid_f, text="⬜", width=3, height=1, #tamanio de las casillas en ancho y alto
+                               font=("Segoe UI Emoji", 14), bg="#f8f9fa", relief="flat") #relief relieve
+                lbl.grid(row=f, column=c, padx=1, pady=1) #Pady distancia de casillas
+                self.celdas[(f, c)] = lbl #Guarda la referencia a ese Label con su posición como llave
 
         self.lbl_msg = tk.Label(self.root, text="Usa W A S D para moverte | H para pista",
-                                bg="#1e1e2e", fg="#cdd6f4", font=("Segoe UI", 10))
-        self.lbl_msg.pack(pady=6)
+                                bg="#bc75e0", fg="#000000", font=("Segoe UI", 10)) #fondo y letras
+        self.lbl_msg.pack(pady=6) # distancia entre la matriz las letras y los botones
 
-        btn_f = tk.Frame(self.root, bg="#1e1e2e")
-        btn_f.pack(pady=4)
+        btn_f = tk.Frame(self.root, bg="#bc75e0") #Caja de widgets = botonones,labels, etc
+        btn_f.pack(pady=4 ) #visualiza los botones
         tk.Button(btn_f, text="💡 Pista", width=8, font=("Segoe UI", 11, "bold"),
-                  bg="#45475a", fg="white", relief="flat",
-                  command=lambda: self._mover("H")).pack(side="left", padx=6)
+                  bg="#280847", fg="white", relief="flat", # letras
+                  command=lambda: self._mover("H")).pack(side="left", padx=6) #llama a la funcion de pistas, los botones no envian a eventos
         tk.Button(btn_f, text="🔄 Reiniciar", width=10, font=("Segoe UI", 11, "bold"),
-                  bg="#1a73e8", fg="white", relief="flat",
-                  command=self._reiniciar).pack(side="left", padx=6)
+                  bg="#0c1145", fg="white", relief="flat",
+                  command=self._reiniciar).pack(side="left", padx=6)#Le dice a Tkinter dónde colocar un widget dentro de su contenedor, y es centrado por defecto
 
         self._dibujar()
 
     def _dibujar(self):
-        for pos, lbl in self.celdas.items():
+        for pos, lbl in self.celdas.items():#.items() te da cada TUPLA del diccionario:
             if pos == self.robot:           lbl.config(text="🤖", bg="#1a73e8")
             elif pos in self.bloqueos:      lbl.config(text="🚫", bg="#ea4335")
             elif pos in self.power:         lbl.config(text="⚡", bg="#fbbc04")
@@ -195,10 +196,10 @@ class RobotGUI:
             else:                           lbl.config(text="⬜", bg="#f8f9fa")
 
     def _mover(self, mov):
-        if self.ganado: return
+        if self.ganado: return#SI SE CUMPLE NO HACE NADA MAS
 
         if mov == "H":
-            self.lbl_msg.config(text=pista(self.robot, self.meta)); return
+            self.lbl_msg.config(text=pista(self.robot, self.meta)); return #CONFIGURA EL MENSAJE PARA QUE SEA LA PISTA
 
         if self.power_activo:
             self.saltos = 2
@@ -207,22 +208,23 @@ class RobotGUI:
             self.saltos = 1
 
         x, y = self.robot
-        for _ in range(self.saltos):
-            if   mov == "D": y = pos_derecha(y)
-            elif mov == "A": y = pos_izquierda(y)
-            elif mov == "S": x = pos_abajo(x)
-            elif mov == "W": x = pos_arriba(x)
+        for _ in range(self.saltos): #no importa el numero de vueltas solo repite n veces
+            if   mov == "D": y = pos_derecha(y) #+
+            elif mov == "A": y = pos_izquierda(y)#-
+            elif mov == "S": x = pos_abajo(x)#+
+            elif mov == "W": x = pos_arriba(x)#-
         self.robot = (x, y)
 
         # Primera verificación de bloqueo
         for i in range(len(self.bloqueos)):
             if self.robot == self.bloqueos[i]:
-                self.lbl_msg.config(text="🚫 Casilla bloqueada!")
-                if   mov == "D": y = pos_derecha_bloqueos(y)
-                elif mov == "A": y = pos_izquierda_bloqueos(y)
-                elif mov == "S": x = pos_abajo_bloqueos(x)
-                elif mov == "W": x = pos_arriba_bloqueos(x)
-                self.robot = (x, y); break
+                self.lbl_msg.config(text="🚫 Casilla bloqueada!") #cambia el mensaje
+                if   mov == "D": y = pos_derecha_bloqueos(y) #-
+                elif mov == "A": y = pos_izquierda_bloqueos(y)#+
+                elif mov == "S": x = pos_abajo_bloqueos(x)#-
+                elif mov == "W": x = pos_arriba_bloqueos(x)#+
+                self.robot = (x, y)
+                break
 
         if self.robot in self.power:
             self.power.remove(self.robot)
@@ -237,9 +239,10 @@ class RobotGUI:
                 elif mov == "A": y = pos_izquierda_bloqueos(y)
                 elif mov == "S": x = pos_abajo_bloqueos(x)
                 elif mov == "W": x = pos_arriba_bloqueos(x)
-                self.robot = (x, y); break
+                self.robot = (x, y)
+                break
 
-        self._dibujar()
+        self._dibujar()# Dibuja el mapa con la nueva posicion del robot
 
         if comprobacion_posicion(self.robot, self.meta):
             self.ganado = True
@@ -252,6 +255,6 @@ class RobotGUI:
         self.lbl_msg.config(text="Usa W A S D para moverte | H para pista")
 
 
-root = tk.Tk()
-RobotGUI(root)
-root.mainloop()
+root = tk.Tk()#Crea la ventana principal del gui
+RobotGUI(root) #Inicia el objeto, todo (botones)
+root.mainloop()#permite que el juego este activo mientras haya eventos
